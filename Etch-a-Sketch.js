ba2936d693b66261b0584f7a50e5a0596d01739e
Elkanah <elkanahawesome@gmail.com>
2022-04-04 15:18:51 -0500

    fixed grid i guess  new ui style

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,25 +1,90 @@
-let gridSize = 20;
+//assigns grid size
+let sliderValue = document.querySelector(".pixelSize");
+let submitNewSize = document.querySelector(".submitNewPixelSize");
+let sliderOutput = document.querySelector(".sliderOutput");
+let gridSelector = document.querySelector(".grids")
+
+//display current grid size
+sliderOutput.textContent = `${sliderValue.value} x ${sliderValue.value}`
+
 const colorPick = document.querySelector("#colorPicker");
 const gridContainer = document.querySelector(".grids");
 
+let height;
+let width;
+
+
+//auto puts grids into gridContainer (runs once)
+gridAppend(sliderValue.value);
+
 //create and add each grid into the container
-for (let entries = 0; entries < gridSize**2; entries++) {
-    let grids = document.createElement('div');
-    grids.classList.add('gridBlock');
-    grids.setAttribute("draggable", "false")
-    grids.style.display = "flex";
-    grids.style.flexGrow = "1";
-    gridContainer.appendChild(grids);
+sliderValue.addEventListener('input', function () {
+    //actively display slider Size.
+    sliderOutput.textContent = `${sliderValue.value} x ${sliderValue.value}`;
+});
+sliderValue.addEventListener('change', function () {
+    //remove grids
+    let removeGrids = document.querySelectorAll(".gridBlock");
+    removeGrids.forEach(grid => {
+        grid.remove();
+    });
+
+    //add grids back
+    gridAppend(sliderValue.value);
+
+    // slider Size.
+    let autoCount = "auto ".repeat(sliderValue.value);
+    gridSelector.style.gridTemplateColumns = autoCount;
+    hovered()
     
+  });
+
+function gridAppend(gridSize) {
+    //does calculation stuff
+    let HeightAndWidth = dimensionCalc(gridSize);
+
+    for (let entries = 0; entries < gridSize**2; entries++) {
+        let createGrids = document.createElement('div');
+        createGrids.classList.add('gridBlock');
+        createGrids.setAttribute("draggable", "false");
+
+
+        
+        //set attrubutes and displays
+        createGrids.style.height = `${height}px`;
+        createGrids.style.width = `${width}px`;
+        gridContainer.appendChild(createGrids);
+    }
+
+    //styling
+    let autoCount = "auto ".repeat(sliderValue.value);
+    gridSelector.style.gridTemplateColumns = autoCount;
+}
+//calculate HEIGHT AND WIDTH
+function dimensionCalc(gridSize) {
+    //get canvas Height And Width
+    let heightCanvas = gridContainer.clientHeight +1;
+    let widthCanvas = gridContainer.clientWidth +1;
+    
+    //calculate Height and Width for individual pixels
+    heightCanvas /= gridSize;
+    widthCanvas  /= gridSize;
+
+    //leaves 1 decimal
+    height = heightCanvas.toFixed(2);
+    width = widthCanvas.toFixed(2);
 }
 
 //select all gridBlocks
-let gridBlocks = document.querySelectorAll(".gridBlock");
+hovered() //auto runs (once)
+function hovered() {
+    let gridBlocks = document.querySelectorAll(".gridBlock");
 
-//detects if is being hovered over
-gridBlocks.forEach(
-    hovered => hovered.addEventListener("mouseover", isHovered)
-);
+    //detects if is being hovered over
+    gridBlocks.forEach(
+        hovered => hovered.addEventListener("mouseover", isHovered)
+    );
+}
 
 //detects if random color button is clicked 
 let randomColorButton = document.querySelector(".randomColors");
@@ -53,25 +118,8 @@ function isHovered() {
 let resetButton = document.querySelector(".reset");
 resetButton.addEventListener("click", resetColor);
 function resetColor() {
+    let gridBlocks = document.querySelectorAll(".gridBlock");
     gridBlocks.forEach(color => {
-        color.style.backgroundColor = "";
+        color.style.backgroundColor = "white";
     });
 }
-
-
-//gridBlocks.forEach(clickDraw => clickDraw.addEventListener("click", clickDrawing));
-/* 
-function clickDrawing() {
-    this.style.backgroundColor = "red";
-} 
-
-//listen to when mouse is hovering over gridBlock
-gridBlocks.forEach(gridColor => gridColor.addEventListener("mouseover", hoverColor));
-
-//change grid color when hovering
-function hoverColor(e) {
-    console.log(e);
-    this.style.backgroundColor = "blue";
-
-
-} */
